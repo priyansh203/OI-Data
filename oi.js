db7c3d3be0ee47066300731eef4eaa6fea7d8da7
priyansh203 <107694625+priyansh203@users.noreply.github.com>
2023-10-18 23:45:02 +0530

    Update oi.js

--- oi.js
+++ oi.js
@@ -1,9 +1,5 @@
 async function getOI(CEPE,strikePrice,index,expiry) {
-  // const index = "banknifty";
   let num = "9"; // for banknifty num="23"
-  // const expiry = "2023-09-28";
-  // const CEPE = "PE";
-  // const strikePrice = "46100.00";
   let OI_long = 0;
 
   if(index==="banknifty"){
@@ -12,7 +8,7 @@ async function getOI(CEPE,strikePrice,index,expiry) {
   else{
     num="9"
   }
-  console.log(num)
+
   
 
   const url = `https://www.moneycontrol.com/india/indexfutures/${index}/${num}/${expiry}/OPTIDX/${CEPE}/${strikePrice}/true`;
@@ -59,12 +55,57 @@ async function getOI(CEPE,strikePrice,index,expiry) {
 
   return OI_long;
 }
+async function getLTP(index){
+  let num="9";
+  let LTP;
+  if(index==="banknifty"){
+    num="23"
+  }
+  else{
+    num="9"
+  }
+  const url = `https://www.moneycontrol.com/india/indexfutures/${index}/${num}?classic=true`;
 
+  try {
+    const response = await fetch(url);
+    
+    if (response.ok) {
+      const html = await response.text();
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, "text/html");
 
+      const divElement = doc.querySelector("div.FL.ML10.MT5");
 
+      if (divElement) {
+        let pElement = divElement.querySelector("p.r_28.FL");
+        if(pElement===null){
+          pElement = divElement.querySelector("p.gr_28.FL");
+        }
+
+        if (pElement) {
+          const cellData = [];
+          const valueObj = pElement.querySelector("strong");
+          const valueText = valueObj.textContent;
+          LTP = parseInt(valueText);
+          // console.log(typeof(LTP));
+          
+        } else {
+          console.log("Table not found within the div.");
+        }
+      } else {
+        console.log("Div element with class 'FR PA10' not found.");
+      }
+    } else {
+      console.log("HTTP request failed with status:", response.status);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+
+  return LTP;
+}
 async function getSumation(CEPE,dataArray,index,expiry){
   let promises = []; // Store promises for each getOI call
-  console.log(dataArray);
   
   for (let i = 0; i < dataArray.length; i++) {
     // Push each getOI call into the promises array
@@ -80,16 +121,71 @@ async function getSumation(CEPE,dataArray,index,expiry){
   return sum;
   
 }
+function checkboxBtnArray(start,end,step){
+  // var start = 42500.00;
+  // var end = 47500.00;
+  // var step = 100.00;
+  var result = [];
 
+  for (var i = start; i <= end; i += step) {
+    result.push(i.toFixed(2).toString());
+  }
+
+return result;
+}
+var checkboxesContainer = document.getElementById("checkboxes");
+function createCheckBox(result){
+  // Create checkboxes for the result array
+  for (var i = 0; i < result.length; i++) {
+    var resultValue = result[i];
+    var checkboxLabel = document.createElement("label");
+    checkboxLabel.classList.add("form-control")
+    checkboxLabel.for = "checkbox" + i;
+    // checkboxLabel.textContent = resultValue;
+     checkboxLabel.innerHTML = resultValue + "&nbsp;";
+
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = "checkboxx";
+    checkbox.name = "Result";
+    checkbox.value = resultValue;
+    checkbox.style.marginRight = "15px";
+
+
+    checkboxesContainer.appendChild(checkboxLabel);
+    checkboxLabel.appendChild(checkbox);
+  }
+}
+
+let interval=null
 function handleGetDataClick() {
   
 
   // Call the getData function with the array as a parameter
   // getSumation(dataArray);
+  clearInterval(interval)
   displayOI();
-  setInterval(displayOI, 3 * 60 * 1000);
+  interval=setInterval(displayOI, 3 * 60 * 1000);
 }
 
+document.getElementById("index").addEventListener("change", function() {
+  var selectedValue = this.value;
+  
+  // Clear any existing checkboxes
+  
+
+  if (selectedValue === "banknifty") {
+    checkboxesContainer.innerHTML = "";
+    let result=checkboxBtnArray(42500.00, 47500.00, 100.00)
+    createCheckBox(result);
+  }
+
+  if (selectedValue === "nifty") {
+    checkboxesContainer.innerHTML = "";
+    var result=checkboxBtnArray(18800.00, 21000.00, 50.00);
+    createCheckBox(result);
+  }
+});
 
 document.getElementById('getDataButton').addEventListener('click', handleGetDataClick);
 
@@ -98,20 +194,23 @@ let prevCE=0
 let prevPE=0
 let prevDiffInOI=0
 
+
+
 async function displayOI() {
   try {
-      // Get the user input from the text field
-  
-    // Split the input string into an array using space as the delimiter
     const index = document.getElementById('index').value;
     const expiry = document.getElementById('expiry').value;
-    const userInput = document.getElementById('textfield').value;
-    const dataArray = userInput.split(' ');
-    
-    
-    
+
+    var selectedCheckboxes = document.querySelectorAll('input[name="Result"]:checked');
+    var dataArray = [];
+
+    for (var i = 0; i < selectedCheckboxes.length; i++) {
+        dataArray.push(selectedCheckboxes[i].value);
+    }
+
     const openInterestCE =  await getSumation("CE",dataArray,index,expiry);
     const openInterestPE = await getSumation("PE",dataArray,index,expiry);
+    const LTP= await getLTP(index);
     const currentTime = new Date().toLocaleTimeString();
     let diffCE=openInterestCE-prevCE
     let diffPE=openInterestPE-prevPE
@@ -143,6 +242,7 @@ let oiTable = document.getElementById('openInterestTable');
         <thead>
           <tr>
             <th>Current time</th>
+            <th>LTP</th>
             <th>CE OI</th>
             <th>PE OI</th>
             <th>Change in CE</th>
@@ -161,6 +261,7 @@ let oiTable = document.getElementById('openInterestTable');
     const newRow = document.createElement('tr');
     newRow.innerHTML = `
       <td>${currentTime}</td>
+      <td>${LTP}</td>
       <td>${openInterestCE}</td>
       <td>${openInterestPE}</td>
       <td class="${diffCE <= 0 ? 'green-bg' : 'red-bg'}">${diffCE}</td>
@@ -208,13 +309,12 @@ function clearData() {
   oiContainer.innerHTML = ''; // Clear displayed divs
 }
 
-// Display the initial data and set an interval to update every 3 minutes
-// displayOI();
-// setInterval(displayOI, 3 * 60 * 1000); // 3 minutes in milliseconds
 
 // Load and display data from local storage when the page loads
 // window.addEventListener('load', loadAndDisplayData);
 
 // Attach event listener to the "Clear" button
-const clearButton = document.getElementById('clearButton');
-clearButton.addEventListener('click', clearData);
+// const clearButton = document.getElementById('clearButton');
+// clearButton.addEventListener('click', clearData);
+
+
